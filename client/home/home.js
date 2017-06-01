6d4a013521c9940cc3629e738540855b7f8193e5
Colton Clemmer <cole.clemmer@gmail.com>
2017-06-01 12:46:52 -0500

    Added total invested column

--- client/home/home.js
+++ client/home/home.js
@@ -1,6 +1,7 @@
 import api from 'api'
 import ko from 'knockout'
 import _ from 'lodash'
+import moment from 'moment'
 
 export default class {
   constructor() {
@@ -12,13 +13,29 @@ export default class {
     api.get('/api/portfolio')
     .then((res) => {
       this.cash = ko.observable(res.cashBalance)
-      this.assets = ko.observableArray(res.assets)
+      this.assets = ko.observableArray(this.getAssets(res.assets))
+      this.timeStamp = ko.observable(moment(new Date(res.timeStamp)).format('MMMM Do YYYY, h:mm:ss a'))
       this.assets.shown = ko.pureComputed(() => _(this.assets())
         .orderBy(this.sort(), this.desc() ? 'desc' : 'asc')
         .filter((a) => new RegExp(this.searchText().toLowerCase()).test(a.symbol.toLowerCase()))
         .value())
+      this.numShares = ko.pureComputed(() => {
+        let count = 0
+        _.each(this.assets(), (a) => count += a.shares)
+        return count
+      })
       this.ready(true)
     })
+  }
+
+  getAssets(assets) {
+    return _(assets)
+      .map((a) => _.defaults(a, {
+          totalInvested: (a.avgCost * a.shares),
+          totalInvestedString: parseFloat((a.avgCost * a.shares).toFixed(2)).toLocaleString(),
+          avgCostString: parseFloat(a.avgCost.toFixed(2)).toLocaleString()
+        }))
+      .value()
   }
 
   sortSymbol() {
@@ -31,6 +48,10 @@ export default class {
 
   sortCost() {
     this.sortChange('avgCost')
+  }
+
+  sortInvested() {
+    this.sortChange('totalInvested')
   }
 
   sortChange(sort) {
